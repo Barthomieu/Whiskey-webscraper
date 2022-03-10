2f98493beb6b6636da298a87ba8476d202b695a2
BartlomiejPlata <bartlomiej,plata@edu.uekat.pl>
2022-03-10 19:02:01 +0100

    test

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 baseurl = "https://shop.whiskybase.com/us/whisky/"
-
+#crawler
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
 productlinks = []
 t={}
