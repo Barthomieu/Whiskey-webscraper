aaff852c3632bc00810114feffb9b62073ff07ff
BartlomiejPlata <bartlomiej,plata@edu.uekat.pl>
2022-04-21 19:27:15 +0200

    view table in webpage

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
+import os
+
 from flask import Flask, request,render_template
 
 from app.db_connection import conn
-
+from flask_assets import Environment, Bundle
 
 
 cursor =conn.cursor()
 app = Flask(__name__)
 
+assets = Environment(app)
+
+assets.load_path = [os.path.join(os.path.dirname(__file__), 'static'),]
+
+# Preprocess scss and bundle CSS
+
+css = Bundle(
+  # Paths to CSS dependencies you don't want to run through scss go here
+  Bundle(
+    'css/styles.scss',
+    filters = 'pyscss',
+    depends = ('**/*.scss', '**/**/*.scss'),
+  ),
+  output = 'dist/app.css'
+)
+
+assets.register('app-css', css)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cursor.execute("SELECT * FROM Whiskey_data")
+    data = cursor.fetchall()
+    return render_template('index.html', data=data)
 @app.route('/mainpage')
 def v_timestamp():
     cursor.execute("SELECT * FROM Whiskey_data")
